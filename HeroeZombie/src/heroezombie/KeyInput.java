fc521d06decf380bf329e54b929151a65e2e108e
nakrull11 <gusguz1994@gmail.com>
2019-02-05 16:25:55 -0300

    movimientos WASD y UP DOWN RIGHT LEFT

--- HeroeZombie/src/heroezombie/KeyInput.java
+++ HeroeZombie/src/heroezombie/KeyInput.java
@@ -29,10 +29,20 @@ public class KeyInput extends KeyAdapter {
             if(tempObject.getId() == ID.Jugador){
               //EVENTOS DE TECLA JUGADOR 1
               
-                if (true) {
-                    
-                }
-              
+                if (key == KeyEvent.VK_W) tempObject.setVelY(-5);
+                if (key == KeyEvent.VK_S) tempObject.setVelY(5);
+                if (key == KeyEvent.VK_D) tempObject.setVelX(5);
+                if (key == KeyEvent.VK_A) tempObject.setVelX(-5);
+            }
+            
+            if (tempObject.getId() == ID.Jugador2) {
+                
+                if (key == KeyEvent.VK_UP) tempObject.setVelY(-5);
+                if (key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
+                if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
+                if (key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
+                
+                
             }
             
         }
@@ -41,6 +51,29 @@ public class KeyInput extends KeyAdapter {
     
     public void keyReleased(KeyEvent e){
         int key = e.getKeyCode();
+        
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            if(tempObject.getId() == ID.Jugador){
+              //EVENTOS DE TECLA JUGADOR 2
+              
+                if (key == KeyEvent.VK_W) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_S) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_D) tempObject.setVelX(0);
+                if (key == KeyEvent.VK_A) tempObject.setVelX(0);
+            }
+            
+            if (tempObject.getId() == ID.Jugador2) {
+                
+                if (key == KeyEvent.VK_UP) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
+                if (key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
+                
+                
+            }
+            
+        }
     }
     
 }
